e0eb52463141c027d20ac7a8e8c471bf706fce2a
leeif <geeklyf92610@gmail.com>
2016-05-07 14:34:04 +0900

    Add Debug

--- middlewares/retrieveBody.go
+++ middlewares/retrieveBody.go
@@ -5,18 +5,17 @@ import (
   "github.com/go-martini/martini"
   "MuShare/utils"
   "reflect"
-  "fmt"
   "log"
 )
 
 func RetrieveBody(typ reflect.Type) martini.Handler {
-  return func (req *http.Request, c martini.Context, logger log.Logger) {
+  return func (req *http.Request, c martini.Context, logger *log.Logger) {
     body := reflect.New(typ).Interface()
-    logger.Println(body)
     c.Map(body)
     err := utils.JsonDecoder(req.Body, body)
     if err != nil {
 
     }
+    logger.Println(body)
   }
 }
